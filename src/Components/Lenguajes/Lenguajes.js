7bb16844062e2855d82fb86e5809a690502ebe0e
AnalieFS <analieflorezs@gmail.com>
2022-06-30 08:27:06 -0500

    experience

--- src/Components/Lenguajes/Lenguajes.js
+++ src/Components/Lenguajes/Lenguajes.js
@@ -20,80 +20,80 @@ export default class Lenguajes extends Component {
     return (
       <div className="lenguajesContainer">
         {this.state.visible && (
-            <div className="panelHerramientas">            
-                <h1 className="panelTitle">¡Clickea el ícono!</h1>
+          <h1 className="panelTitle">¡Clickea el ícono!</h1>
+        )}
 
-          <div className="herramientas">
-            <div className="python">
-              <h1>Python</h1>
-              <h2>Avanzado</h2>
-            </div>
-            <div className="java">
-              <h1>Java</h1>
-              <h2>Avanzado</h2>
-            </div>
-            <div className="javascript">
-              <h1>JavaScript</h1>
-              <h2>Intermedio</h2>
-            </div>
-            <div className="html">
-              <h1>HTML</h1>
-              <h2>Intermedio</h2>
-            </div>
-            <div className="css">
-              <h1>CSS</h1>
-              <h2>Intermedio</h2>
-            </div>
-            <div className="mysql">
-              <h1>MySQL</h1>
-              <h2>Intermedio</h2>
-            </div>
-            <div className="heidisql">
-              <h1>HeidiSQL</h1>
-              <h2>Intermedio</h2>
-            </div>
-            <div className="postgresql">
-              <h1>Postgresql</h1>
-              <h2>Intermedio</h2>
-            </div>
-            <div className="docker">
-              <h1>Docker</h1>
-              <h2>Básico</h2>
-            </div>
-            <div className="react">
-              <h1>React JS</h1>
-              <h2>Intermedio</h2>
-            </div>
-            <div className="node">
-              <h1>Node JS</h1>
-              <h2>Intermedio</h2>
-            </div>
-            <div className="typescript">
-              <h1>TypeScript</h1>
-              <h2>Intermedio</h2>
-            </div>
-            <div className="linux">
-              <h1>Linux</h1>
-              <h2>Intermedio</h2>
-            </div>
-            <div className="vs">
-              <h1>VS</h1>
-              <h2>Avanzado</h2>
-            </div>
-            <div className="vscode">
-              <h1>VS Code</h1>
-              <h2>Avanzado</h2>
-            </div>
-            <div className="pycharm">
-              <h1>PyCharm</h1>
-              <h2>Intermedio</h2>
-            </div>
-            <div className="git">
-              <h1>Github</h1>
-              <h2>Básico</h2>
-            </div>
-          </div>          </div>
-
+        {this.state.visible && (
+            <div className="herramientas">
+              <div className="python">
+                <h1>Python</h1>
+                <h2>Avanzado</h2>
+              </div>
+              <div className="java">
+                <h1>Java</h1>
+                <h2>Avanzado</h2>
+              </div>
+              <div className="javascript">
+                <h1>JavaScript</h1>
+                <h2>Intermedio</h2>
+              </div>
+              <div className="html">
+                <h1>HTML</h1>
+                <h2>Intermedio</h2>
+              </div>
+              <div className="css">
+                <h1>CSS</h1>
+                <h2>Intermedio</h2>
+              </div>
+              <div className="mysql">
+                <h1>MySQL</h1>
+                <h2>Intermedio</h2>
+              </div>
+              <div className="heidisql">
+                <h1>HeidiSQL</h1>
+                <h2>Intermedio</h2>
+              </div>
+              <div className="postgresql">
+                <h1>Postgresql</h1>
+                <h2>Intermedio</h2>
+              </div>
+              <div className="docker">
+                <h1>Docker</h1>
+                <h2>Básico</h2>
+              </div>
+              <div className="react">
+                <h1>React JS</h1>
+                <h2>Intermedio</h2>
+              </div>
+              <div className="node">
+                <h1>Node JS</h1>
+                <h2>Intermedio</h2>
+              </div>
+              <div className="typescript">
+                <h1>TypeScript</h1>
+                <h2>Intermedio</h2>
+              </div>
+              <div className="linux">
+                <h1>Linux</h1>
+                <h2>Intermedio</h2>
+              </div>
+              <div className="vs">
+                <h1>VS</h1>
+                <h2>Avanzado</h2>
+              </div>
+              <div className="vscode">
+                <h1>VS Code</h1>
+                <h2>Avanzado</h2>
+              </div>
+              <div className="pycharm">
+                <h1>PyCharm</h1>
+                <h2>Intermedio</h2>
+              </div>
+              <div className="git">
+                <h1>Github</h1>
+                <h2>Básico</h2>
+              </div>
+          </div>
         )}
         {!this.state.visible && (
           <div className="titleLenguajes">
