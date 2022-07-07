d7fd7f6399c210a1e948c38c9779744b2a352c85
AnalieFS <analieflorezs@gmail.com>
2022-07-07 11:27:36 -0500

    Footer form

--- src/Components/Footer/Contacto.js
+++ src/Components/Footer/Contacto.js
@@ -5,29 +5,65 @@ import send from "../../img/send.svg";
 import "./Contacto.css";
 
 export default class Contacto extends Component {
- 
+  state = {
+    email: "",
+    comment: "",
+  };
 
-  change = () =>{
+  change = () => {
     window.location.href = "/PoliticaPrivacidad";
+  };
+
+  emailChange = (e) => {
+    this.setState({ email: e.target.value });
+  };
+
+  commentChange = (e) => {
+    this.setState({ comment: e.target.value });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const postData = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.email,
+        comment: this.state.comment,
+      }),
+    };
+    fetch(
+      "https://analieflorezsanchezcvbackend.herokuapp.com/api/contact",
+      postData
+    ).then((response) => response.json()).then(()=>{
+      this.setState({email : ""});
+      this.setState({comment : ""});
+      window.alert("¡Formulario enviado con éxito!");
+    }
+    );
   }
+
   render() {
     return (
       <footer className="footer">
         <div className="letsTalk">
           <h1>¡Hablemos!</h1>
           <h2>Envíe su correo y le contactaré lo antes posible</h2>
-          <form className="form">
-              <div>
-            <input type="email" required></input>
-            <button id="send">
-              <img src={send} alt="enviar"></img>
-            </button>            
+          <form className="form" onSubmit={this.onSubmit}>
+            <div>
+              <input type="email" required value= {this.state.email} onChange={this.emailChange} placeholder="Correo Electrónico"></input>
+              <input type="text" required value= {this.state.comment} onChange={this.commentChange} placeholder="¿De qué quieres hablar?"></input>
+              <button id="send" onClick={this.sendForm}>
+                <img src={send} alt="enviar"></img>
+              </button>
             </div>
             <div>
-            <input required type="checkbox"></input>
-            <label id="terminos">Acepto los términos y condiciones que puedo encontrar en las políticas de privacidad.</label>
+              <input required type="checkbox"></input>
+              <label id="terminos">
+                Acepto los términos y condiciones que puedo encontrar en las
+                políticas de privacidad.
+              </label>
             </div>
-            
           </form>
         </div>
         <div className="menuFooter">
@@ -43,13 +79,23 @@ export default class Contacto extends Component {
 
           <div className="line"></div>
           <div className="redesSociales">
-<a href="https://www.instagram.com" target="_blank"><h2>INSTAGRAM</h2></a>
-            <a href="https://www.linkedin.com/in/analie-fl%C3%B3rez-s%C3%A1nchez/" target="_blank"><h2>LINKEDIN</h2></a>
-            <a href="https://github.com/AnalieFS" target="_blank"><h2>GITHUB</h2></a>
+            <a href="https://www.instagram.com" target="_blank">
+              <h2>INSTAGRAM</h2>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/analie-fl%C3%B3rez-s%C3%A1nchez/"
+              target="_blank"
+            >
+              <h2>LINKEDIN</h2>
+            </a>
+            <a href="https://github.com/AnalieFS" target="_blank">
+              <h2>GITHUB</h2>
+            </a>
           </div>
-
         </div>
-        <h2 className='privacidad' onClick={this.change}>POLÍTICAS DE PRIVACIDAD</h2>
+        <h2 className="privacidad" onClick={this.change}>
+          POLÍTICAS DE PRIVACIDAD
+        </h2>
       </footer>
     );
   }
