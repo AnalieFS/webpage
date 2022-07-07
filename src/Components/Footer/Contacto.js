297f205cb4c49300d567aa164b6762af3d139794
AnalieFS <analieflorezs@gmail.com>
2022-07-07 13:22:10 -0500

    Experiencia

--- src/Components/Footer/Contacto.js
+++ src/Components/Footer/Contacto.js
@@ -2,6 +2,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
 import send from "../../img/send.svg";
+import wp from '../../img/social/wp.svg';
+import email from '../../img/social/mail.svg';
 import "./Contacto.css";
 
 export default class Contacto extends Component {
@@ -35,44 +37,62 @@ export default class Contacto extends Component {
     fetch(
       "https://analieflorezsanchezcvbackend.herokuapp.com/api/contact",
       postData
-    ).then((response) => response.json()).then(()=>{
-      this.setState({email : ""});
-      this.setState({comment : ""});
-      window.alert("¡Formulario enviado con éxito!");
-    }
-    );
-  }
+    )
+      .then((response) => response.json())
+      .then(() => {
+        this.setState({ email: "" });
+        this.setState({ comment: "" });
+        window.alert("¡Formulario enviado con éxito!");
+      });
+  };
 
   render() {
     return (
       <footer className="footer">
         <div className="letsTalk">
           <h1>¡Hablemos!</h1>
-          <h2>Envíe su correo y le contactaré lo antes posible</h2>
+          <h2>Envía el siguiente formulario y te contactaré lo antes posible</h2>
           <form className="form" onSubmit={this.onSubmit}>
-            <div>
-              <input type="email" required value= {this.state.email} onChange={this.emailChange} placeholder="Correo Electrónico"></input>
-              <input type="text" required value= {this.state.comment} onChange={this.commentChange} placeholder="¿De qué quieres hablar?"></input>
+            <div id="TerminosYCondiciones0">
+              <input
+                type="email"
+                required
+                value={this.state.email}
+                onChange={this.emailChange}
+                placeholder="Correo Electrónico"
+              ></input>
+              <input
+                type="text"
+                required
+                value={this.state.comment}
+                onChange={this.commentChange}
+                placeholder="Menciona el tema a tratar"
+              ></input>
               <button id="send" onClick={this.sendForm}>
                 <img src={send} alt="enviar"></img>
               </button>
             </div>
-            <div>
+            <div id="TerminosYCondiciones">
               <input required type="checkbox"></input>
               <label id="terminos">
                 Acepto los términos y condiciones que puedo encontrar en las
-                políticas de privacidad.
+                políticas de privacidad que puedo encontrar al final de esta página.
               </label>
             </div>
           </form>
         </div>
         <div className="menuFooter">
           <div className="tel">
+            <a  href="https://wa.link/dohv2i"
+              target="_blank">
+            <img src={wp} alt="WhatsApp Icon"/>
             <h1>Escríbame</h1>
             <h2>(+57) 300 445 2451</h2>
+            </a>
           </div>
           <div className="line"></div>
           <div className="email">
+          <img src={email} alt="Email Icon"/>
             <h1>Envíeme un correo</h1>
             <h2>analieflorezs@gmail.com</h2>
           </div>
